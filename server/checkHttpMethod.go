61e3b37884289313b8d5da5331dda4e02fe06798
Rafael Idrisov <rafael.idrisov@gmail.com>
2025-01-30 23:11:46 +0300

    Home PC 30.01.2025 23:11 check methods

---
+++ server/checkHttpMethod.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func checkHttpMethod(w http.ResponseWriter, r *http.Request) error {
+	matchMethods := map[string]string{
+		"/create": http.MethodPost,
+		"/read":   http.MethodGet,
+		"/update": http.MethodPut,
+		"/delete": http.MethodDelete,
+		"/exists": http.MethodGet,
+	}
+
+	for path, method := range matchMethods {
+		if path == r.URL.Path && method != r.Method {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "Method %s not allowed for path %s, should be %s", r.Method, path, method)
+			fmt.Fprintf(w, "Method %s not allowed for path %s, should be %s", r.Method, path, method)
+			log.Printf("Method %s not allowed for path %s, should be %s", r.Method, path, method)
+			return
+		}
+	}
+
+}
