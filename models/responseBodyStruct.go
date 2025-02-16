c97cee33dfe3d365aaffcc603011f55c568aee6c
Rafael Idrisov <rafael.idrisov@gmail.com>
2025-02-16 18:07:17 +0300

    Home PC 16.02.2025 18:07

--- models/responseBodyStruct.go
+++ models/responseBodyStruct.go
@@ -1,9 +1,50 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"net/http"
+)
 
 type ResponseBody struct {
-	Result  sql.Result
-	Rows    sql.Rows
+	// Meta info
+	Method        string // http.Request.Method
+	OperationType string // one of the CRUD
+
+	// Errors
+	EntryErrors map[*CrudEntry]error
+	BadIdxs     []int
+
+	// Info for 'CREATE', 'UPDATE' and 'DELETE'
+	Result       sql.Result
+	LastID       int64
+	RowsAffected int64
+
+	// Info for 'READ'
+	Rows    *sql.Rows
 	Columns []string
+	Entries []Entry // []map[string]string
+
+	// Error handling
+	Error error
+}
+
+type Entry map[string]string
+
+func (rb *ResponseBody) ProcessResult() error {
+	if rb.Result != nil {
+		var err error
+
+		if rb.OperationType == "CREATE" || rb.Method == http.MethodPost {
+			rb.LastID, err = rb.Result.LastInsertId()
+			if err != nil {
+				return err
+			}
+		}
+
+		rb.RowsAffected, err = rb.Result.RowsAffected()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
