ac01b5a12257d180966edb6ec91bd6da4911adaf
Rafael Idrisov <rafael.idrisov@gmail.com>
2025-03-09 15:18:04 +0300

    Home PC 09.03.2025 total app rewrite

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pgproxy
+
+go 1.23.3
--- server/existsRoute.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import (
-	"net/http"
-)
-
-func existsRecord(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
