1b272c68155ea59eabae0be300a5d3a9d3dd2482
Rafael Idrisov <rafael.idrisov@gmail.com>
2025-03-23 13:45:27 +0300

    Home PC 23.03.2025 13:45

---
+++ internal/handlers/health.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// The healthCheck function is designed to check the server's availability and readiness via an HTTP endpoint.
+// It can be used in various scenarios such as:
+//  1. Liveness Probes: Ensures that the server is running and responding to requests,
+//     which is useful for container orchestrators like Kubernetes.
+//  2. Readiness Probes: Checks if the server is ready to handle incoming traffic by verifying its internal state.
+//  3. Load Balancing & Monitoring: Used by load balancers or monitoring systems to ensure the server is available.
+//
+// Example usage:
+// In Kubernetes, periodic checks are made against the `/health` endpoint. If the server responds with status code 200 OK,
+// it is considered healthy and ready to serve traffic.
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		errorResponse(w, http.StatusMethodNotAllowed, "Method not allowed")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+}
